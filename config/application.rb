8f9bb042103a85709d5c9f4919b3de0624678260
Zheng Gong <zheng.gong@gree.net>
2020-03-03 16:54:24 +0900

    add corf

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,15 @@ module Blog
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Permit cross origin
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+      origins "*"
+      resource "*",
+      headers: :any,
+      methods: [:get, :post, :options, :head]
+      end
+    end
   end
 end
