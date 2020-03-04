0592af761b6439726265fe7bc82fedb038ad5c36
Zheng Gong <zheng.gong@gree.net>
2020-03-04 19:17:34 +0900

    add micropost

---
+++ app/controllers/api/v1/microposts_controller.rb
@@ -0,0 +1,48 @@
+module Api
+    module V1
+      class MicropostsController  < ApplicationController
+        before_action :set_micropost, only: [:show, :update, :destroy]
+  
+        def index
+          microposts = Micropost.order(created_at: :desc)
+          render json: { status: 'SUCCESS', message: 'Loaded microposts', data: microposts }
+        end
+  
+        def show
+          render json: { status: 'SUCCESS', message: 'Loaded the micropost', data: @micropost }
+        end
+  
+        def create
+          micropost = Micropost.new(micropost_params)
+          if micropost.save
+            render json: { status: 'SUCCESS', data: micropost }
+          else
+            render json: { status: 'ERROR', data: micropost.errors }
+          end
+        end
+  
+        def destroy
+          @micropost.destroy
+          render json: { status: 'SUCCESS', message: 'Deleted the micropost', data: @micropost }
+        end
+  
+        def update
+          if @micropost.update(micropost_params)
+            render json: { status: 'SUCCESS', message: 'Updated the micropost', data: @micropost }
+          else
+            render json: { status: 'SUCCESS', message: 'Not updated', data: @micropost.errors }
+          end
+        end
+  
+        private
+  
+        def set_micropost
+          @micropost = Micropost.find(params[:id])
+        end
+  
+        def micropost_params
+          params.require(:micropost).permit(:content, :user_id)
+        end
+      end
+    end
+  end
