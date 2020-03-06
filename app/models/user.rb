89b93e9dd786c2bb8209098a2de76ff0d653ea6f
Zheng Gong <zheng.gong@gree.net>
2020-03-06 17:07:17 +0900

    add password digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
     has_many :microposts, dependent: :destroy
+    has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_044417) do
+ActiveRecord::Schema.define(version: 2020_03_06_072242) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_03_06_044417) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password"
+    t.string "password_digest"
   end
 
 end
