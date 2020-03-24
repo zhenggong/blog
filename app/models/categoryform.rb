bad3aa695a96dcc9a8095dc10b11523ee2c8b7b5
Zheng Gong <zheng.gong@gree.net>
2020-03-24 19:29:42 +0900

    delete embeded model

--- app/models/categoryform.rb
+++ app/models/categoryform.rb
@@ -3,6 +3,4 @@ class Categoryform
   store_in collection: "mongodb_blog_development", client: "default"
   field :title, type: String
   field :element, type: Hash
-  embedded_in :category
-  embedded_in :user
 end
